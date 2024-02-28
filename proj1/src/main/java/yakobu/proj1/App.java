4ac172b5bfbdbbee1eedcbbe4acc42382783c06f
user <user@DESKTOP-RTFTBKU>
2024-02-28 22:24:14 +0530

    add project

---
+++ proj1/src/main/java/yakobu/proj1/App.java
@@ -0,0 +1,12 @@
+package yakobu.proj1;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("java is awesome");
+    }
+}
