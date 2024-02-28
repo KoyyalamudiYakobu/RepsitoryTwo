2946600cc77e92667a6b541893572f583e0a0af1
user <user@DESKTOP-RTFTBKU>
2024-02-28 22:37:37 +0530

    Tl--add mul method

--- proj1/src/main/java/yakobu/proj1/ArithematicOperations.java
+++ proj1/src/main/java/yakobu/proj1/ArithematicOperations.java
@@ -5,6 +5,19 @@ public class ArithematicOperations
 
 	public int sum(int x,int y) 
 	{
+		System.out.println(x+y);
 		return x+y;
+	}
+	
+	public int sub(int x,int y)
+	{
+		System.out.println(x-y);
+		return x-y;
+	}
+	
+	
+	public int mul( int x,int y) {
+		System.out.println(x*y);
+		return x*y;
 	}
 }
