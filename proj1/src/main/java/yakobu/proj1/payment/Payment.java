badbf179c1968416eb5eaea5d6b7b95367e5cc6a
user <user@DESKTOP-RTFTBKU>
2024-02-28 22:54:39 +0530

    dev1-- add some methods in payment class

--- proj1/src/main/java/yakobu/proj1/payment/Payment.java
+++ proj1/src/main/java/yakobu/proj1/payment/Payment.java
@@ -1,5 +1,12 @@
 package yakobu.proj1.payment;
 
-public class Payment {
+public class Payment 
+{
 
+	public void creditCardpayment() {
+		System.out.println(" this is credetCard paymnent method");
+	}
+	public void upipayment() {
+		System.out.println(" this is upi paymnent method");
+	}
 }
